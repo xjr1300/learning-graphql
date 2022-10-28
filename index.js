7a8d711f93388f06d467cc2af5cfa85faf4db91f
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-10-28 15:22:44 +0900

    5.2.4.1

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const { ApolloServer } = require(`apollo-server`);
 
 // スキーマ定義
 const typeDefs = `
+  # User型
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
+
   # PhotoCategory列挙型定義
   enum PhotoCategory {
     SELFIE
@@ -18,6 +26,7 @@ const typeDefs = `
     name: String!
     category: PhotoCategory!
     description: String
+    postedBy: User!
   }
 
   input PostPhotoInput {
@@ -36,10 +45,43 @@ const typeDefs = `
   }
 `;
 
+// cSpell: disable
+// 登録済みユーザー
+let users = [
+  { githubLogin: "mHattrup", name: "Mike Hattrrup" },
+  { githubLogin: "gPlake", name: "Glen Plake" },
+  { githubLogin: "sSchmidt", name: "Scot Schmidt" },
+];
+// cSpell: enable
+
 // 投稿された写真に付与するID
-let _id = 0;
-// 写真を格納する配列
-let photos = [];
+let _id = 3;
+
+// cSpell: disable
+// 投稿された写真
+let photos = [
+  {
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake",
+  },
+  {
+    id: "2",
+    name: "Enjoying the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt",
+  },
+  {
+    id: "3",
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt",
+  },
+];
+// cSpell: enable
 
 // リゾルバ定義
 const resolvers = {
@@ -65,6 +107,15 @@ const resolvers = {
 
   Photo: {
     url: (parent) => `http://yooursite.com/img/${parent.id}.jpg`,
+    postedBy: (parent) => {
+      return users.find((u) => u.githubLogin === parent.githubUser);
+    },
+  },
+
+  User: {
+    postedPhotos: (parent) => {
+      return photos.filter((p) => p.githubUser === parent.githubLogin);
+    },
   },
 };
 
