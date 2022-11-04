4820dbee1d3044e70e0d3b0c6631d9f89804385b
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-11-04 16:37:15 +0900

    サブスクリプションをリゾルバに追加

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const { readFileSync } = require("fs");
 
-const { ApolloServer } = require(`apollo-server-express`);
+const { ApolloServer } = require("apollo-server-express");
+const { PubSub } = require("graphql-subscriptions");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const express = require("express");
 const expressPlayground =
   require("graphql-playground-middleware-express").default;
-const express = require("express");
-
-const { createServer } = require("http");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
-const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
-const { WebSocketServer } = require("ws");
 const { useServer } = require("graphql-ws/lib/use/ws");
+const { createServer } = require("http");
+const { WebSocketServer } = require("ws");
+const { MongoClient } = require("mongodb");
 
 const typeDefs = readFileSync("./typeDefs.graphql", "utf-8");
 const resolvers = require("./resolvers");
 
-const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 const start = async () => {
-  // Expressアプリケーションを作成
-  let app = express();
+  // データベースと接続
+  const { MONGO_HOST, MONGO_PORT, MONGO_DB_NAME } = process.env;
+  const mongoUrl = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`; // Expressアプリケーションを作成
+  const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true });
+  const db = client.db();
 
   // WebSocketを提供するサーバーを構築
+  let app = express();
   const httpServer = createServer(app);
   const wsServer = new WebSocketServer({
     server: httpServer,
@@ -30,24 +34,20 @@ const start = async () => {
   const schema = makeExecutableSchema({ typeDefs, resolvers });
   const serverCleanup = useServer({ schema }, wsServer);
 
-  // データベースと接続
-  const { MONGO_HOST, MONGO_PORT, MONGO_DB_NAME } = process.env;
-  const mongoUrl = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`;
-  const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true });
-  const db = client.db();
-
   // サーバー・インスタンスを構築して起動
   // スキーマ、リゾルバ及びコンテキストを引数で与える。
+  const pubsub = new PubSub();
   const server = new ApolloServer({
     schema,
-    context: async ({ req }) => {
-      const githubToken = req.headers.authorization;
+    context: async ({ req, connection }) => {
+      const githubToken = req
+        ? req.headers.authorization
+        : connection.context.Authorization;
       const currentUser = await db.collection("users").findOne({ githubToken });
-      return { db, currentUser };
+      return { db, currentUser, pubsub };
     },
     plugins: [
       ApolloServerPluginDrainHttpServer({ httpServer }),
-      // Proper shutdown for the WebSocket server.
       {
         async serverWillStart() {
           return {
@@ -60,8 +60,6 @@ const start = async () => {
     ],
   });
   await server.start();
-
-  // サーバーにExpressミドルウェアを追加
   server.applyMiddleware({ app });
 
   // ホーム・ルートを設定
@@ -72,7 +70,7 @@ const start = async () => {
   // サーバーを起動
   app.listen({ port: 4000 }, () =>
     console.log(
-      `GraphQL Server running @ http://localhost::4000${server.graphqlPath}`
+      `GraphQL Server running at http://localhost::4000${server.graphqlPath}`
     )
   );
 };
